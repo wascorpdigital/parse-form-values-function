5c260d74cbce45074b8f506114941de6d89f0efb
nimit patel <nimit2997@gmail.com>
2020-11-05 18:28:32 +0530

    text based form data

---
+++ getformdata.js
@@ -0,0 +1,74 @@
+export const getFormData = (form) => {
+    const formData = {}
+
+    if (!form || form.nodeName !== "FORM") {
+        return;
+    }
+
+    for (let index = 0; index < form.elements.length; index++) {
+        if (form.elements[index].name === "") {
+            continue;
+        }
+        switch (form.elements[index].nodeName) {
+            case 'INPUT':
+                switch (form.elements[index].type) {
+                    case 'text':
+                    case 'hidden':
+                    case 'email':
+                    case 'password':
+                    case 'button':
+                    case 'reset':
+                    case 'submit':
+                        formData[form.elements[index].name] = form.elements[index].value;
+                        break;
+                    case 'radio':
+                        if (form.elements[index].checked) {
+                            formData[form.elements[index].name] = form.elements[index].value;
+                        }
+                        break;
+                }
+            case 'checkbox':
+                if (form.elements[index].checked) {
+                    if (form.elements[index].name in formData) {
+                        formData[form.elements[index].name] = formData[form.elements[index].name] + ',' + form.elements[index].value;
+                    }
+                    else {
+                        formData[form.elements[index].name] = form.elements[index].value;
+                    }
+                }
+                break;
+            case 'file':
+                break;
+            case 'TEXTAREA':
+                formData[form.elements[index].name] = form.elements[index].value;
+                break;
+            case 'SELECT':
+                switch (form.elements[index].type) {
+                    case 'select-one':
+                        formData[form.elements[index].name] = form.elements[index].value;
+                        break;
+                    case 'select-multiple':
+                        const multi = []
+                        for (let i = 0; i < form.elements[index].options.length; i++) {
+                            if (form.elements[index].options[i].selected) {
+                                multi.push(form.elements[index].options[i].value)
+                                formData[form.elements[index].name] = multi;
+                            }
+                        }
+                        break;
+                }
+                break;
+            case 'BUTTON':
+                switch (form.elements[index].type) {
+                    case 'reset':
+                    case 'submit':
+                    case 'button':
+                        formData[form.elements[index].name] = form.elements[index].value;
+                        break;
+                }
+                break;
+        }
+    }
+
+    return formData
+}
